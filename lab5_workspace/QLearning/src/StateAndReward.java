1bc7babdc0f074187ecd37c37dcee3d7045c0c58
InspectorDeno <dennisdufback@gmail.com>
2018-12-26 22:18:25 +0100

    Add Lab 5. Part 2 done

---
+++ lab5_workspace/QLearning/src/StateAndReward.java
@@ -0,0 +1,107 @@
+public class StateAndReward {
+	
+	private static final int NUM_ANGLE_STATES = 30;
+	private static final int OPTIMAL_ANGLE_STATE = NUM_ANGLE_STATES/2;
+	
+	/* State discretization function for the angle controller */
+	public static String getStateAngle(double angle, double vx, double vy) {
+
+		/* TODO: IMPLEMENT THIS FUNCTION */
+
+		int discrete = discretize2(angle, NUM_ANGLE_STATES, -3, 3);
+//		System.out.println(discrete);
+		return Integer.toString(discrete);
+	}
+
+	/* Reward function for the angle controller */
+	public static double getRewardAngle(double angle, double vx, double vy) {
+		
+		double reward = Math.pow(OPTIMAL_ANGLE_STATE - 
+				Math.abs(OPTIMAL_ANGLE_STATE - discretize2(angle, NUM_ANGLE_STATES, -2, 2)), 2) - 
+				Math.pow(OPTIMAL_ANGLE_STATE/2, 2);
+		System.out.println("reward: " + reward);
+		return reward;
+		}
+	
+	/* State discretization function for the full hover controller */
+	public static String getStateHover(double angle, double vx, double vy) {
+
+		/* TODO: IMPLEMENT THIS FUNCTION */
+
+		String state = "OneStateToRuleThemAll2";
+		
+		return state;
+	}
+
+	/* Reward function for the full hover controller */
+	public static double getRewardHover(double angle, double vx, double vy) {
+
+		/* TODO: IMPLEMENT THIS FUNCTION */
+		
+		double reward = 0;
+
+		return reward;
+	}
+
+	// ///////////////////////////////////////////////////////////
+	// discretize() performs a uniform discretization of the
+	// value parameter.
+	// It returns an integer between 0 and nrValues-1.
+	// The min and max parameters are used to specify the interval
+	// for the discretization.
+	// If the value is lower than min, 0 is returned
+	// If the value is higher than min, nrValues-1 is returned
+	// otherwise a value between 1 and nrValues-2 is returned.
+	//
+	// Use discretize2() if you want a discretization method that does
+	// not handle values lower than min and higher than max.
+	// ///////////////////////////////////////////////////////////
+	public static int discretize(double value, int nrValues, double min,
+			double max) {
+		if (nrValues < 2) {
+			return 0;
+		}
+
+		double diff = max - min;
+
+		if (value < min) {
+			return 0;
+		}
+		if (value > max) {
+			return nrValues - 1;
+		}
+
+		double tempValue = value - min;
+		double ratio = tempValue / diff;
+
+		return (int) (ratio * (nrValues - 2)) + 1;
+	}
+
+	// ///////////////////////////////////////////////////////////
+	// discretize2() performs a uniform discretization of the
+	// value parameter.
+	// It returns an integer between 0 and nrValues-1.
+	// The min and max parameters are used to specify the interval
+	// for the discretization.
+	// If the value is lower than min, 0 is returned
+	// If the value is higher than min, nrValues-1 is returned
+	// otherwise a value between 0 and nrValues-1 is returned.
+	// ///////////////////////////////////////////////////////////
+	public static int discretize2(double value, int nrValues, double min,
+			double max) {
+		double diff = max - min;
+
+		if (value < min) {
+			return 0;
+		}
+		if (value > max) {
+			return nrValues - 1;
+		}
+
+		double tempValue = value - min;
+		double ratio = tempValue / diff;
+
+		return (int) (ratio * nrValues);
+	}
+
+}
